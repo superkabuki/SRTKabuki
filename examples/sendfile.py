1973aa65d80dd424d4e7ad6268e057bc75c03ba9
Super Kabuki <a@iodisco.com>
2025-11-06 01:16:46 -0500

    Update sendfile.py

--- examples/sendfile.py
+++ examples/sendfile.py
@@ -7,39 +7,47 @@ from ctypes.util import find_library
 from srtkabuki import SRTKabuki
 from sockopts import SRTO_TRANSTYPE
 
-SRT_ERROR = -1
-SRT_INVALID_SOCK = -1
 
-srtk = SRTKabuki("0.0.0.0", 9000)
-yes = 1
-srtk.setsockflag(SRTO_TRANSTYPE, yes)
-srtk.bind()
-srtk.listen()
-# Accept multiple client connections (infinite loop simulation)
-print("Waiting for connections...")
-
-while True:
+def send_a_file(srtk):
+    """
+    send_a_file accept a connection,
+    read filename size from socket,
+    read filename from socket,
+    sendfile to client
+    close connection
+    """
     fhandle = srtk.accept() 
     print(f"Accepted new connection (socket ID: {fhandle})...")
     buffer_size = 8
-    data = srtk.recv(
-        buffer_size,
-        fhandle,
-    )
-    print(data)
+    data = srtk.recv(buffer_size,fhandle)
     filenamesize = int(data)
     print(filenamesize)
-    filename = srtk.recv(
-        filenamesize,
-        fhandle,
-    )
+    filename = srtk.recv(filenamesize,fhandle)
     print(filename)
-    srtk.getlasterror()
     srtk.sendfile(filename, fhandle)
     time.sleep(1)
     srtk.close(fhandle)
-    srtk.getlasterror()
+   
+
+def go():
+    """
+    go create an SRTKabuki instance,
+    set the sock flag for files,
+    bind it to 0.0.0.0 port 9000,
+    accept connections
+    """
+    srtk = SRTKabuki("0.0.0.0", 9000)
+    yes = 1
+    srtk.setsockflag(SRTO_TRANSTYPE, yes)
+    srtk.bind()
+    srtk.listen()
+    print("Waiting for connections...")
+    while True:
+        send_a_file(srtk)
 
 
-if __name__ == "__main__":
-    main()
+if __name__=='__main__':
+    go()
+    
+
+

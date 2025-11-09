d860579fca87c997d58124c9632ea1e695bf77dd
Super Kabuki <a@iodisco.com>
2025-11-09 16:02:13 -0500

    Update recvfile.py

--- examples/recvfile.py
+++ examples/recvfile.py
@@ -8,10 +8,9 @@ https://github.com/Haivision/srt/blob/master/examples/recvfile.cpp
 import sys
 from srtkabuki import SRTKabuki
 
-addr = sys.argv[1]
-port = sys.argv[2]
-remote_file = sys.argv[3]
-local_file = sys.argv[4]
+srt_url = sys.argv[1] # srt://example.com:9000
+remote_file = sys.argv[2]
+local_file = sys.argv[3]
 
-srtk=SRTKabuki(addr=addr,port=port)
+srtk=SRTKabuki(srt_url)
 srtk.fetch(remote_file,local_file)

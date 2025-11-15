8bb4370836d6ee23faa64cbd2e4ea708285be426
Super Kabuki <a@iodisco.com>
2025-11-15 00:39:15 -0500

    Update srtscte35.py

--- srtscte35.py
+++ srtscte35.py
@@ -1,40 +1,38 @@
 #!/usr/bin/env python3
 
 import sys
-import time
-from  srtkabuki import SRTKabuki
-from threefive import Stream,Cue
+from srtkabuki import SRTKabuki
+from threefive import Stream, Cue
 
-PACKETSIZE=188
-BUFFSIZE=1456
-SYNC_BYTE=b'G'
+
+PACKETSIZE = 188
+BUFFSIZE = 1316
+SYNC_BYTE = b"G"
+
 
 def spinner(lc):
     """
     cli spinner to let you know things are running.
     """
-    spin_map= {79:  ' |', 77: ' /',73: ' -',71: ' \\',}
-   
-    for k,v in spin_map.items():
-        if lc % k == 0:
-            print(v,'', end='\r')
-            if lc %79 == 0:
-                lc = 0
+    spins = ["\\", "-", "/", "|"]
+    lc %= len(spins)
+    print(spins[lc], file=sys.stderr, end="\r")
 
-def sync_byte(stuff):
+
+def sync_byte(data):
     """
     sync_byte check stuff for sync_byte
     """
-    return stuff[0:1] == SYNC_BYTE
+    return data[0:1] == SYNC_BYTE
 
 
-def parse_packet(packet,strm):
+def parse_packet(packet, strm):
     """
     parse_packet check mpegts packet for scte35
     """
     if sync_byte(packet):
         if len(packet) == PACKETSIZE:
-            cue=strm._parse(packet)
+            cue = strm._parse(packet)
             if cue:
                 Cue(packet).show()
 
@@ -46,20 +44,13 @@ def packetize(data):
     return [data[i : i + PACKETSIZE] for i in range(0, len(data), PACKETSIZE)]
 
 
-def iter_packets(data,strm):
-    """
-    iter_packets iterate mpegts packets
-    """
-    _= [parse_packet(packet,strm) for packet in  packetize(data)]
-
-
 def parse_mpegts(data, strm):
     """
     parse_mpegts split data into packets
     """
-    if len(data) >=  PACKETSIZE:
+    if len(data) >= PACKETSIZE:
         if sync_byte(data):
-            iter_packets(data,strm)
+            _=[parse_packet(packet, strm) for packet  in packetize(data)]
 
 
 def preflight():
@@ -69,21 +60,20 @@ def preflight():
     """
     kabuki = SRTKabuki(sys.argv[1])
     kabuki.connect()
-    buffsize=1456
+    buffsize = 1456
     buffer = kabuki.mkbuff(BUFFSIZE)
     strm = Stream(tsdata=None)
     return kabuki, buffer, strm
-    
 
-if __name__ =='__main__':
-    kabuki,buffer,strm = preflight()
-    lc =0
-    data=b''
+
+if __name__ == "__main__":
+    kabuki, buffer, strm = preflight()
+    lc = 0
+    data = b""
     while True:
         st = kabuki.recvmsg(buffer)
-        data=buffer.raw
+        data = buffer.raw
         spinner(lc)
-        lc +=1
+        lc += 1
         buffer = kabuki.mkbuff(BUFFSIZE)
         parse_mpegts(data, strm)
-

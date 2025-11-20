ce7351fb7373ba1c27f383499d1afee8f47286a7
Super Kabuki <a@iodisco.com>
2025-11-20 01:09:55 -0500

    working

--- examples/sendfile.py
+++ examples/sendfile.py
@@ -16,19 +16,19 @@ def send_a_file(srtk):
     sendfile to client
     close connection
     """
-    fhandle = srtk.accept() 
+    fhandle = srtk.accept()
     print(f"Accepted new connection (socket ID: {fhandle})...")
-    smallbuff=srtk.mkbuff(8)
-    srtk.recv(smallbuff,fhandle)
-    filenamesize = int(smallbuff)
+    smallbuff = srtk.mkbuff(1316)
+    srtk.recv(smallbuff, fhandle)
+    filenamesize = smallbuff.value
     print(filenamesize)
-    filename =srtk.mkbuff(filenamesize)
-    srtk.recv(filename,fhandle)
-    print(filename.raw)
-    srtk.sendfile(filename.raw, fhandle)
+    fbuff = srtk.mkbuff(1316)
+    srtk.recv(fbuff, fhandle)
+    srtk.sendfile(fbuff.value, fhandle)
     time.sleep(1)
     srtk.close(fhandle)
-   
+    srtk.getlasterror()
+
 
 def go():
     """
@@ -37,7 +37,7 @@ def go():
     bind it to 0.0.0.0 port 9000,
     accept connections
     """
-    srtk = SRTKabuki('srt://0.0.0.0:9000')
+    srtk = SRTKabuki("srt://0.0.0.0:9000")
     yes = 1
     srtk.setsockflag(SRTO_TRANSTYPE, yes)
     srtk.bind()
@@ -47,8 +47,5 @@ def go():
         send_a_file(srtk)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     go()
-    
-
-

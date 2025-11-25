fdd39271dd528d1f4bdbb3d2307ccc805a99f1f2
Super Kabuki <a@iodisco.com>
2025-11-25 12:42:21 -0500

    Add files via upload

---
+++ examples/sendfile.py
@@ -0,0 +1,57 @@
+import ctypes
+import sys
+import os
+import time
+import socket
+from ctypes.util import find_library
+from srtkabuki import SRTKabuki
+from srtkabuki.sockopts import SRTO_TRANSTYPE
+from srtkabuki.static import SRT_FILE
+
+
+def send_a_file(srtk):
+    """
+    send_a_file accept a connection,
+    read filename size from socket,
+    read filename from socket,
+    sendfile to client
+    close connection
+    """
+    fhandle = srtk.accept()
+    srtk.getsockstate()
+    srtk.getsockstate(fhandle)
+    print(f"Accepted new connection (socket ID: {fhandle})...")
+    smallbuff = srtk.mkbuff(1316)
+    srtk.recv(smallbuff, fhandle)
+    filenamesize = int.from_bytes(smallbuff.value, byteorder="little")
+    print("filenamesize ", filenamesize)
+    fbuff = srtk.mkbuff(filenamesize)
+    srtk.recv(fbuff, fhandle)
+    try:
+        srtk.sendfile(fbuff.value, fhandle)
+    except:
+        print(f"File {fbuff.value.decode()} Not Found.")
+    finally:
+        time.sleep(1)
+        srtk.close(fhandle)
+        srtk.getlasterror()
+
+
+def go():
+    """
+    go create an SRTKabuki instance,
+    set the sock flag for files,
+    bind it to 0.0.0.0 port 9000,
+    accept connections
+    """
+    srtk = SRTKabuki("srt://0.0.0.0:9000")
+    srtk.setsockflag(SRTO_TRANSTYPE, SRT_FILE)
+    srtk.bind()
+    srtk.listen()
+    print("Waiting for connections...")
+    while True:
+        send_a_file(srtk)
+
+
+if __name__ == "__main__":
+    go()
---
+++ examples/test-c-client.py
@@ -0,0 +1,27 @@
+"""
+This is SRTKabuki version of examples/test-c-client.c in libsrt.
+https://github.com/Haivision/srt/blob/master/examples/test-c-client.c
+"""
+
+import sys
+import time
+from srtkabuki import SRTKabuki
+from srtkabuki.sockopts import SRTO_SENDER
+
+
+def main():
+    srt_url = sys.argv[1]  # srt://example.com:9000
+    srtk = SRTKabuki(srt_url)
+    srtk.setsockflag(SRTO_SENDER, 1)
+    srtk.connect()
+    a = 100
+    while a:
+        a -= 1
+        srtk.sendmsg2(b"I am super cool")
+        time.sleep(0.01)
+    time.sleep(1)
+    srtk.close()
+    srtk.cleanup()
+
+
+main()

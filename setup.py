5d6dbd38b25544a70831c44005cc8e3bb4640d7e
Adrian of Doom <a@iodisco.com>
2025-11-30 02:28:11 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+from srtfu.version import version
+
+
+setuptools.setup(
+    name="SRTfu",
+    version=version,
+    author="Adrian of Doom ",
+    author_email="spam@bad.show",
+    description="Secure Reliable Transport in Python",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/superkabuki/SRTfu",
+    install_requires=[
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: Sleepycat License",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX :: BSD :: OpenBSD",
+        "Operating System :: POSIX :: BSD :: NetBSD",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Multimedia :: Video",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    python_requires=">=3.9",
+)

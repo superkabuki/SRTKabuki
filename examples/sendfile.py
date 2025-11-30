6ea18331fa201af09d5378af16a49c901b6569d3
Adrian of Doom <a@iodisco.com>
2025-11-30 05:19:09 -0500

    imports

--- examples/sendfile.py
+++ examples/sendfile.py
@@ -5,8 +5,8 @@ import time
 import socket
 from ctypes.util import find_library
 from srtfu import SRTfu
-from srtfu.srt_h import SRTO_TRANSTYPE
-from srtfu.srt_h import SRT_FILE
+from srtfu import SRTO_TRANSTYPE
+from srtfu import SRT_FILE
 
 
 def send_a_file(srtk):
@@ -17,24 +17,24 @@ def send_a_file(srtk):
     sendfile to client
     close connection
     """
-    fhandle = srtk.accept()
-    srtk.getsockstate()
-    srtk.getsockstate(fhandle)
+    fhandle = srtf.accept()
+    srtf.getsockstate()
+    srtf.getsockstate(fhandle)
     print(f"Accepted new connection (socket ID: {fhandle})...")
-    smallbuff = srtk.mkbuff(1316)
-    srtk.recv(smallbuff, fhandle)
+    smallbuff = srtf.mkbuff(1316)
+    srtf.recv(smallbuff, fhandle)
     filenamesize = int.from_bytes(smallbuff.value, byteorder="little")
     print("filenamesize ", filenamesize)
-    fbuff = srtk.mkbuff(filenamesize)
-    srtk.recv(fbuff, fhandle)
+    fbuff = srtf.mkbuff(filenamesize)
+    srtf.recv(fbuff, fhandle)
     try:
-        srtk.sendfile(fbuff.value, fhandle)
+        srtf.sendfile(fbuff.value, fhandle)
     except:
         print(f"File {fbuff.value.decode()} Not Found.")
     finally:
         time.sleep(1)
-        srtk.close(fhandle)
-        srtk.getlasterror()
+        srtf.close(fhandle)
+        srtf.getlasterror()
 
 
 def go():
@@ -44,13 +44,13 @@ def go():
     bind it to 0.0.0.0 port 9000,
     accept connections
     """
-    srtk = SRTfu("srt://0.0.0.0:9000")
-    srtk.setsockflag(SRTO_TRANSTYPE, SRT_FILE)
-    srtk.bind()
-    srtk.listen()
+    srtf = SRTfu("srt://0.0.0.0:9000")
+    srtf.setsockflag(SRTO_TRANSTYPE, SRT_FILE)
+    srtf.bind()
+    srtf.listen()
     print("Waiting for connections...")
     while True:
-        send_a_file(srtk)
+        send_a_file(srtf)
 
 
 if __name__ == "__main__":

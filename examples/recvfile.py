f8b25c00cd74479d0b6fb2788ce4377c09c786b4
Adrian of Doom <a@iodisco.com>
2025-12-01 14:19:14 -0500

    cleanup

--- examples/recvfile.py
+++ examples/recvfile.py
@@ -6,10 +6,13 @@ https://github.com/Haivision/srt/blob/master/examples/recvfile.cpp
 """
 
 import sys
-from srtfu import fetch
+from srtfu import fetch, SRTO_TRANSTYPE, SRT_FILE
 
-srt_url = sys.argv[1]  # srt://example.com:9000
-remote_file = sys.argv[2]
-local_file = sys.argv[3]
 
-fetch(srt_url , remote_file, local_file)
+if __name__ == '__main__':
+    srt_url = sys.argv[1]  # srt://example.com:9000
+    remote_file = sys.argv[2]
+    local_file = sys.argv[3]
+
+    flags={SRTO_TRANSTYPE: SRT_FILE,}
+    fetch(srt_url , remote_file, local_file, flags)

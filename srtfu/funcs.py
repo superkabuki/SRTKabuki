ef445efc4091f904da5c43325d6b95724b685540
Adrian of Doom <a@iodisco.com>
2025-11-30 20:19:52 -0500

    Add files via upload

---
+++ srtfu/funcs.py
@@ -0,0 +1,77 @@
+"""
+funcs.py
+
+Home of the fetch and datagramer functions.
+
+"""
+import sys
+import time
+from .srtfu import SRTfu
+from . import SRTO_TRANSTYPE, SRTO_RCVSYN, SRTO_RCVBUF, SRT_LIVE
+
+
+PACKETSIZE = 188
+BUFFSIZE = 1456
+
+
+def _setflags(srtf, flags):
+    """
+    _setflags set flags on an SRTfu instance
+
+    srtf SRTfu instance
+    flags  a dict of socket flags you want to have set.
+               ex. {SRTO_TRANSTYPE: SRT_LIVE,
+                       SRTO_RCVSYN: 1, }
+    """
+    for k,v in flags:
+        srtf.setsockflag(k,v)
+
+
+def _preflight(srt_url, flags):
+    """
+    preflight init SRTfu instance,
+    and set sock flags as desired.
+    """
+    srtf= SRTfu(srt_url)
+    _setflags(srtf, flags)
+    srtf.connect()
+    return srtf
+
+
+def datagramer(srt_url,flags=None):
+    """
+    datagramer datagram generator
+    take a live srt_url
+    and return datagrams
+    """
+    preflags ={SRTO_TRANSTYPE: SRT_LIVE,
+                       SRTO_RCVSYN: 1,
+                       SRTO_RCVBUF: 32768,}
+    if flags:
+        preflags.update(flags)
+    srtf = _preflight(srt_url,preflags)
+    while True:
+        buffer = srtf.mkbuff(BUFFSIZE)
+        st = srtf.recv(buffer)
+        datagram = buffer.raw
+        yield datagram
+
+
+
+def fetch(srt_url, remote_file,local_file, flags=None):
+    """
+    fetch retrive a file over srt.
+
+    srt_url   ex. srt://10.0.0.1:9000
+    remote_file  ex.  /this/that/file.ts
+    local_file    ex. /home/me/file.ts
+    flags  a dict of socket flags you want to have set.
+               in most situations this is not needed.
+               ex. {SRTO_TRANSTYPE: SRT_LIVE,
+                       SRTO_RCVSYN: 1, }
+    """
+    preflags = {}
+    if flags:
+        preflags.update(flags)
+    srtf = _preflight(srt_url,preflags)
+    srtf.fetch(remote_file, local_file)

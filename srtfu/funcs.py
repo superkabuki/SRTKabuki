bfa2963d538889b2a375d0451a89f2ef412ecf07
Adrian of Doom <a@iodisco.com>
2025-12-19 19:55:28 -0500

    fixed packetizer from dropping packets

--- srtfu/funcs.py
+++ srtfu/funcs.py
@@ -4,13 +4,14 @@ funcs.py
 Home of the fetch and datagramer functions.
 
 """
+
 import sys
 import time
 from .srtfu import SRTfu
 from . import SRTO_TRANSTYPE, SRTO_RCVSYN, SRTO_RCVBUF, SRT_LIVE
 
 
-BUFFSIZE = 1456
+BUFFSIZE = 1500
 PKTSZ = 188
 SYNC_BYTE = b"G"
 ZERO = b"\x00"
@@ -21,27 +22,30 @@ def _preflight(srt_url, flags=None):
     preflight init SRTfu instance,
     and set sock flags as desired.
     """
-    srtf= SRTfu(srt_url,flags)
+    srtf = SRTfu(srt_url, flags)
+    srtf.conlive()
     srtf.connect()
     return srtf
 
 
-def datagramer(srt_url,flags=None):
+def datagramer(srt_url, flags=None):
     """
     datagramer datagram generator
     take a live srt_url
     and return datagrams
     """
-    preflags ={SRTO_TRANSTYPE: SRT_LIVE,
-                       SRTO_RCVSYN: 1,
-                       SRTO_RCVBUF: 32768000,}
+    preflags = {
+        SRTO_TRANSTYPE: SRT_LIVE,
+        SRTO_RCVSYN: 1,
+        SRTO_RCVBUF: 32768000,
+    }
     if flags:
         preflags.update(flags)
-    srtf = _preflight(srt_url,preflags)
+    srtf = _preflight(srt_url, preflags)
     while True:
         buffer = srtf.mkbuff(BUFFSIZE)
         st = srtf.recv(buffer)
-        datagram = buffer.raw
+        datagram = buffer.raw#.rstrip(ZERO)
         yield datagram
 
 
@@ -59,18 +63,7 @@ def at_least_a_packet(stuff):
     return len(stuff) >= PKTSZ
 
 
-def verified(packet):
-    """
-    verified  checks length of
-    packet and that it starts with
-    a sync byte
-    """
-    if at_least_a_packet(packet):
-        return has_sync_byte(packet)
-    return False
-
-
-def packetizer(srt_url,flags=None):
+def packetizer(srt_url, flags=None):
     """
     packetizer  mpegts packet generator
 
@@ -78,17 +71,17 @@ def packetizer(srt_url,flags=None):
         for packet in packetizer(srt_url):
             ...
     """
-    bigfatbuff=b'' 
-    for datagram in datagramer(srt_url,flags):
-        bigfatbuff += datagram.rstrip(ZERO)
+    bigfatbuff = b""
+    for datagram in datagramer(srt_url, flags):
+        bigfatbuff += datagram
         while SYNC_BYTE in bigfatbuff:
             bigfatbuff = bigfatbuff[bigfatbuff.index(SYNC_BYTE) :]
             packet, bigfatbuff = bigfatbuff[:PKTSZ], bigfatbuff[PKTSZ:]
-            if verified(packet):
+            if has_sync_byte(packet):
                 yield packet
 
 
-def fetch(srt_url, remote_file,local_file, flags=None):
+def fetch(srt_url, remote_file, local_file, flags=None):
     """
     fetch retrive a file over srt.
 
@@ -100,5 +93,5 @@ def fetch(srt_url, remote_file,local_file, flags=None):
                ex. {SRTO_TRANSTYPE: SRT_LIVE,
                        SRTO_RCVSYN: 1, }
     """
-    srtf = SRTfu(srt_url,flags)
+    srtf = SRTfu(srt_url, flags)
     srtf.fetch(remote_file, local_file)
